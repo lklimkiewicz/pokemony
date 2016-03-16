16846725722effe1944672c01ea3d89e78ba0804
Łukasz Klimkiewicz <klimkiewicz.lukasz@gmail.com>
2016-03-16 06:15:05 +0100

    Initial Commit

---
+++ pokemony/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokemony
+//
+//  Created by Lukasz Klimkiewicz on 16.03.2016.
+//  Copyright © 2016 Lukasz Klimkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

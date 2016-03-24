94e733b655d30b816e6a07577fce3cfcdc89e723
Lukasz Klimkiewicz <lklimkiewicz@Mac-mini-Lukasz.local>
2016-03-24 05:50:47 +0100

    detail pokemon view added

---
+++ pokemony/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokemony
+//
+//  Created by Lukasz Klimkiewicz on 18.03.2016.
+//  Copyright © 2016 Lukasz Klimkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokemony/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokemony
+//
+//  Created by Lukasz Klimkiewicz on 17.03.2016.
+//  Copyright © 2016 Lukasz Klimkiewicz. All rights reserved.
+//
+
+import Foundation
